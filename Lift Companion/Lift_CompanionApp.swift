f1309521eb96461dac982200b2153d87ee535d29
Eric <eric@bipa.app>
2024-07-13 23:48:27 -0300

    Start project

---
+++ Lift Companion/Lift_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lift_CompanionApp.swift
+//  Lift Companion
+//
+//  Created by Eric on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Lift_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
